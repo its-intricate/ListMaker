52bb9cd56cdd74915cbc529f72c857ea799ce934
Esther Ogunmefun <esther.ogunmefun@bglgroup.co.uk>
2021-01-13 10:00:19 +0000

    Initial Commit

---
+++ app/src/main/java/com/raywenderlich/listmaker/ListDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.raywenderlich.listmaker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class ListDetailFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
